d9ab3896f3aa1023bf3695c8ce92b470a79397c7
David Baldwynn <whitef0x0@users.noreply.github.com>
2015-06-11 15:45:29 -0700

    Create lab5.vhd

---
+++ lab5.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity lab5 is
+  port(CLOCK_50            : in  std_logic;
+       KEY                 : in  std_logic_vector(3 downto 0);
+       SW                  : in  std_logic_vector(17 downto 0);
+       VGA_R, VGA_G, VGA_B : out std_logic_vector(9 downto 0);  -- The outs go to VGA controller
+       VGA_HS              : out std_logic;
+       VGA_VS              : out std_logic;
+       VGA_BLANK           : out std_logic;
+       VGA_SYNC            : out std_logic;
+       VGA_CLK             : out std_logic);
+end lab5;
+
+architecture RTL of lab5 is
+begin
+
+end RTL;
+
