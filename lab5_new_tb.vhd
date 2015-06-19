71dfadf8f1bb652800d616e653757307e9afadb9
David Baldwynn <whitef0x0@users.noreply.github.com>
2015-06-18 19:32:56 -0700

    Got testbench working in modelsim

--- lab5_new_tb.vhd
+++ lab5_new_tb.vhd
@@ -8,6 +8,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
+use ieee.std_logic_signed.all;
 
 -- Testbenches don't have input and output ports.  We'll talk about that in class
 -- later in the course.
@@ -39,12 +40,12 @@ architecture stimulus of lab5_new_tb is
     --Input/Output mapping signals
     signal clk: std_logic := '0';
     signal key : std_logic_vector(3 downto 0);
-    signal sw : std_logic_vector(8 downto 0);
+    signal sw : std_logic_vector(17 downto 0);
     -- signal led_out : std_logic_vector(7 downto 0);
     signal colour_out : std_logic_vector(2 downto 0);
     signal x_out : std_logic_vector(7 downto 0);
     signal y_out : std_logic_vector(6 downto 0);
-    signal plot_out : std_logic
+    signal plot_out : std_logic;
     
     --Component Signals
       --KEY Signals
@@ -60,8 +61,8 @@ architecture stimulus of lab5_new_tb is
     --type record_name is
     --  output_record
     --    colour : std_logic_vector(2 downto 0);
-    --    x : std_logic_vector(7 downto 0);
-    --    y : std_logic_vector(6 downto 0);
+    --    x_out : std_logic_vector(7 downto 0);
+    --    y_out : std_logic_vector(6 downto 0);
     --    plot :  std_logic;
     --  end record;
     
@@ -83,7 +84,8 @@ architecture stimulus of lab5_new_tb is
   -- between clock edges
 
   --Period of 50MHz Clock
-	constant HALF_PERIOD : time := 10ns;
+	--constant HALF_PERIOD : Time := 10ns;
+	constant HALF_PERIOD : Time := 1ps;
 
 	--Colour constants
 	constant WALL_COLOUR : std_logic_vector(2 downto 0) := "111";
@@ -92,16 +94,16 @@ architecture stimulus of lab5_new_tb is
 	constant P2_COLOUR : std_logic_vector(2 downto 0) := "100";
 	
 	--Position constants
-	constant INIT_X_PUCK : std_logic_vector(7 downto 0) := "01010000"; --80
-	constant INIT_Y_PUCK : std_logic_vector(7 downto 0) := "0111100"; --60
+	constant INIT_X_PUCK : unsigned(7 downto 0) := "01010000"; --80
+	constant INIT_Y_PUCK : unsigned(6 downto 0) := "0111100"; --60
 	
 	--constant INIT_X_DRAW : std_logic_vector(7 downto 0) := "00000101"; --5
 	--constant INIT_Y_DRAW : std_logic_vector(7 downto 0) := "0000101"; --5
 	
 begin
 
-	key <= resetb & "00" & "00" & "00";
-	sw <= p2_fwd & p2_goalie & "0000 0000 0000 00" & p2_fwd & p2_goalie;
+	key <= resetb & "000";
+	sw <= p2_fwd & p2_goalie & "00000000000000" & p2_fwd & p2_goalie;
 
 	DUT: lab5_new 
 		port map (
@@ -110,122 +112,140 @@ begin
       sw => sw,
       -- ledg => out_led,
       colour_out => colour_out,
-      x_out x_out,
+      x_out => x_out,
       y_out => y_out,
       plot_out => plot_out
     );
   
   -- CLOCK STIMULI
-	CLOCK: process
+	clock_process: process is
 	begin
-		CLK <= not clk after HALF_PERIOD ns;
-		wait for 2*HALF_PERIOD ns;
+		CLK <= not clk after HALF_PERIOD;
+		wait for 2*HALF_PERIOD;
 	end process; 
 	
-  main_test: process is
-    variable t1g, t1f, t2g, t2f: : unsigned(6 downto 0);
+  test: process is
+    variable t1g, t1f, t2g, t2f: unsigned(6 downto 0);
 		variable puckx : unsigned(7 downto 0) := INIT_X_PUCK; -- 80
     variable pucky : unsigned(6 downto 0) := INIT_Y_PUCK; -- 60
     variable velx : std_logic := '0';
     variable vely : std_logic := '0';
 
+    variable tmp_i_y : integer := 0;
+    variable tmp_i_x : integer := 0;
     -- procedure test_sequence(
     --     input_sequence: input_record_array;
     --     expected_output_sequence: output_record_array
     -- ) is begin
     --     for i in input_sequence'range loop
-    --         x <= input_sequence(i);
+    --         x_out <= input_sequence(i);
     --         wait until rising_edge(clk);
     --         assert colour_out = expected_output_sequence(i).colour;
-    --         --assert x_out = expected_output_sequence(i).x;
-    --         --assert y_out = expected_output_sequence(i).y;
+    --         --assert x_out = expected_output_sequence(i).x_out;
+    --         --assert y_out = expected_output_sequence(i).y_out;
     --         assert plot_out = expected_output_sequence(i).plot;
     --     end loop;
     -- end;
     
-    procedure clear_screen() is 
+    procedure clear_screen is 
     begin
 
 			wait until rising_edge(clk);
-			
-			assert x_out = "00000000";
-      assert y_out = "0000000";
-      assert plot_out = '1';
-      assert colour_out = BACKGROUND_COLOUR;
+			assert x_out = "00000000" report "x not initialized to 0" severity failure;
+      assert y_out = "0000000" report "y not initialized to 0run" severity failure;
+      assert plot_out = '0' report "plot != 0 " severity failure;
+      assert colour_out = BACKGROUND_COLOUR report "color not 000 " severity failure;
+      wait until rising_edge(clk);
       wait until rising_edge(clk);
 			
 			for i_y in 0 to 119 loop
-				for i_x in 1 to 159 loop
-					assert plot_out = '1';
+				for i_x in 1 to 160 loop
+				  tmp_i_x := i_x;
+				  tmp_i_y := i_y;
+				  --report "plot_out is "& std_logic'image( plot_out );
+					assert plot_out = '1' report "plot not 1 CLRSCRN. plot_out:"& std_logic'image( plot_out) &" i_x:"&integer'image( i_x)& " i_y:"& integer'image( i_y) severity failure;
 					-- assert x_out = std_logic_vector(i_x);
 					-- assert y_out = std_logic_vector(i_y);
-					assert colour_out = "000";
+					assert colour_out = BACKGROUND_COLOUR report "color not 000 CLRSCRN" severity failure;
 					wait until rising_edge(clk);
 				end loop;
+				 
 			end loop;
-    end;
+			--report "END CLR SCRN\n";
+			--report "plot_out:"& std_logic'image( plot_out) &" i_x:"&integer'image( tmp_i_x)& " i_y:"& integer'image( tmp_i_y);
+      --report "colour_out:"& integer'image( conv_integer(colour_out) ) &" x_out:"&integer'image( conv_integer(x_out) )& " y_out:"& integer'image( conv_integer(y_out) );
+
+    end clear_screen;
     
-    procedure draw_walls() is 
+    procedure draw_walls is 
     begin
     
+      --wait until rising_edge(clk);
 			--Check we are starting in correct state
-			assert plot_out = '1';
-			assert colour_out = WALL_COLOUR; -- make sure colour of walls is white
+			--assert plot_out = '1' ;
+			assert colour_out = WALL_COLOUR report "colour_out not WALL_COLOUR (111)" severity failure; -- make sure colour of walls is white
 			
 			-- make sure we start at (5,5)
-			assert x_out = "00000101"; 
-			assert y_out = "0000101";
-		
+			--report "x_out is "& integer'image( conv_integer(x_out) );
+			assert x_out = "00000101" report "x_out not 5" severity failure; 
+			assert y_out = "0000101" report "y_out not 5" severity failure;
+		  
+		  wait until rising_edge(clk);
 	
 			--Draw top wall
-			for i in 5 to 154 loop
-				assert plot_out = '1';
+			for i in 6 to 154 loop
+			  --report "plot_out is "& std_logic'image( plot_out );
+				assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
 				-- assert x_out = std_logic_vector(i);
 				-- assert y_out = "0000101";
-				assert colour_out = WALL_COLOUR;
+				assert colour_out = WALL_COLOUR report "colour_out not WALL_COLOUR (111)" severity failure;
 				wait until rising_edge(clk);
 			end loop;
 			
+			
+			--report "colour_out:"& integer'image( conv_integer(colour_out) ) &" x_out:"&integer'image( conv_integer(x_out) )& " y_out:"& integer'image( conv_integer(y_out) );
+
 			-- make sure we start at (5,155)
-			assert x_out = "00000101"; 
-			assert y_out = "1110011";
+			assert x_out = "00000101" report "x_out not 5" severity failure; 
+			assert y_out = "1110011" report "x_out not 115" severity failure;
+			
+			wait until rising_edge(clk);
+			
+			--report "colour_out:"& integer'image( conv_integer(colour_out) ) &" x_out:"&integer'image( conv_integer(x_out) )& " y_out:"& integer'image( conv_integer(y_out) );
+
 			
 			--Draw bottom wall
-			for i in 5 to 154 loop
-				assert plot_out = '1';
-				assert x_out = "00000101";
-				if(i >= 54 and i <= 66) then
-					assert colour_out = P1_COLOUR;
-				elsif(i >= 115) then
-					assert colour_out = "
-				-- assert x_out = std_logic_vector(i);
-				-- assert y_out = "1110011";
-				assert colour_out = WALL_COLOUR;
+			for i in 6 to 154 loop
+				assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
+				assert x_out = conv_std_logic_vector(i,x_out'length) report "x_out not i. x_out:"&integer'image(conv_integer(x_out))&" i:"&integer'image(i) severity failure;
+				assert y_out = "1110011" report "x_out not 115" severity failure;
+				assert colour_out = WALL_COLOUR report "colour_out not WALL_COLOUR (111)" severity failure;
 				wait until rising_edge(clk);
+
 			end loop;
-    end;
+    end draw_walls;
    
-		procedure draw_paddles() is 
+		procedure draw_paddles is 
     begin
 	  --STEP #1	
 			--test s1g state (DRAW PLYR1 GOALIE)
 				
 				--Start drawing from (5,6)
-			assert x_out = "00000101"; -- 5
-			assert y_out = "0000110"; -- 6
-			assert colour_out = BACKGROUND_COLOUR;
-			assert plot_out = '1';
+			assert x_out = "00000101" report "x_out not 5" severity failure; -- 5
+			assert y_out = "0000110" report "x_out not 5" severity failure; -- 6
+			assert colour_out = BACKGROUND_COLOUR  report "colour_out not BACKGROUND_COLOUR (000)" severity failure;
+			assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
 			wait until rising_edge(clk);
 			
 				--run s1g state (DRAW PLYR2 GOALIE)
 			for i in 7 to 115 loop
-				assert plot_out = '1';
-				if(i >= 54 and i <= 66) then
-					assert colour_out = P1_COLOUR;
+				assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
+				if((i >= 54) and (i <= 66)) then
+					assert colour_out = P1_COLOUR  report "colour_out not P1_COLOUR" severity failure;
 	      elsif(i = 115) then
-					assert colour_out = WALL_COLOUR;
+					assert colour_out = WALL_COLOUR report "colour_out not WALL_COLOUR (111)" severity failure;
 	      else 
-	      	assert colour_out = BACKGROUND_COLOUR;
+	      	assert colour_out = BACKGROUND_COLOUR  report "colour_out not BACKGROUND_COLOUR (000)" severity failure;
 				end if;
 				
 				wait until rising_edge(clk);
@@ -235,20 +255,21 @@ begin
 	  --STEP #2
 	    --test s1f state (DRAW PLYR1 FWD)
 	    assert x_out = "01000011"; -- 67
-	    assert y_out = "0000110"; -- 6
-	    assert colour_out = BACKGROUND_COLOUR;
-	    assert plot_out = '1';
+	    assert y_out = "0000110" report "x_out not 5" severity failure; -- 6
+	    assert colour_out = BACKGROUND_COLOUR  report "BACKGROUND_COLOUR" severity failure;
+	    assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
 	    wait until rising_edge(clk);
 	
 			--run s1f state (DRAW PLYR1 FWD)
 			for i in 7 to 115 loop
-				assert plot_out = '1';
+				assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
 				if(i >= 54 and i <= 66) then
-					assert colour_out = P1_COLOUR;
+					assert colour_out = P1_COLOUR report  "colour_out not P2_COLOUR" severity failure;
 	      elsif(i = 115) then
-					assert colour_out = WALL_COLOUR;
+					assert colour_out = WALL_COLOUR report "colour_out not WALL_COLOUR (111)" severity failure;
 	      else 
-	      	assert colour_out = BACKGROUND_COLOUR;
+	      	assert colour_out = BACKGROUND_COLOUR report "colour_out not BACKGROUND_COLOUR (000)" severity failure;
+
 				end if;
 				
 				wait until rising_edge(clk);
@@ -258,20 +279,22 @@ begin
 		--STEP #3
 			--test s2g state (DRAW PLYR2 GOALIE)
 	    assert x_out = "10011010"; -- 154
-	    assert y_out = "0000110"; -- 6
-	    assert colour_out = BACKGROUND_COLOUR;
-	    assert plot_out = '1';
+	    assert y_out = "0000110" report "x_out not 5" severity failure; -- 6
+	    assert colour_out = BACKGROUND_COLOUR report "colour_out not BACKGROUND_COLOUR (000)" severity failure;
+	    assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
 	    wait until rising_edge(clk);
 	
 				--run s2g state (DRAW PLYR2 GOALIE)
 			for i in 7 to 115 loop
-				assert plot_out = '1';
+				assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
 				if(i >= 54 and i <= 66) then
-					assert colour_out = P2_COLOUR;
+					assert colour_out = P2_COLOUR report "colour_out not P2_COLOUR" severity failure;
+
 				elsif(i = 115) then
-					assert colour_out = WALL_COLOUR;
+					assert colour_out = WALL_COLOUR report "colour_out not WALL_COLOUR (111)" severity failure;
 	      else 
-	      	assert colour_out = BACKGROUND_COLOUR;
+	      	assert colour_out = BACKGROUND_COLOUR report "colour_out not BACKGROUND_COLOUR (000)" severity failure;
+
 				end if;
 				
 				wait until rising_edge(clk);
@@ -281,20 +304,21 @@ begin
 		--STEP #4
 			--test s2f state (DRAW PLYR2 FWD)
 	    assert x_out = "01011101"; -- 93
-	    assert y_out = "0000110"; -- 6
-	    assert colour_out = BACKGROUND_COLOUR;
-	    assert plot_out = '1';
+	    assert y_out = "0000110" report "x_out not 5" severity failure; -- 6
+	    assert colour_out = BACKGROUND_COLOUR  report "colour_out not BACKGROUND_COLOUR (000)" severity failure;
+	    assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
 	    wait until rising_edge(clk);
 	
 				--run s2g state (DRAW PLYR2 GOALIE)
 			for i in 7 to 115 loop
-				assert plot_out = '1';
+				assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
 				if(i >= 54 and i <= 66) then
-					assert colour_out = P2_COLOUR;
+					assert colour_out = P2_COLOUR  report "colour_out not P2_COLOUR" severity failure;
 	      elsif(i = 115) then
-					assert colour_out = WALL_COLOUR;
+					assert colour_out = WALL_COLOUR report "colour_out not WALL_COLOUR (111)" severity failure;
 	      else 
-	      	assert colour_out = BACKGROUND_COLOUR;
+	      	assert colour_out = BACKGROUND_COLOUR report "colour_out not BACKGROUND_COLOUR (000)" severity failure;
+
 				end if;
 				
 				wait until rising_edge(clk);
@@ -303,19 +327,19 @@ begin
 			
 		--STEP #5
 			--test sgp1 state (INIT PUCK)
-			assert plot_out = '1';
-	    assert colour_out = BACKGROUND_COLOUR;
-	    assert x = INIT_X_PUCK; -- 80
-	    assert y = INIT_Y_PUCK; -- 60
+			assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
+	    assert colour_out = BACKGROUND_COLOUR report "colour not BACKGROUND_COLOUR" severity failure;
+	    assert x_out = std_logic_vector(INIT_X_PUCK) report "x_out not INIT_X_PUCK (80)" severity failure; -- 80
+	    assert y_out = std_logic_vector(INIT_Y_PUCK) report"y_out not INIT_Y_PUCK (60)" severity failure; -- 60
+	    
 	    wait until rising_edge(clk);
-	end;
+	end draw_paddles;	
+	
 begin
 
 --Test #1: Move All Paddles DOWN
-		
-	--Test 1 | STEP #1
-		
-    --Initialize inputs
+
+--Initialize inputs
 		p2_fwd <= '0';
 		p2_goalie <= '0';
 		p1_fwd <= '0';
@@ -337,11 +361,11 @@ begin
 		
 		--Clear Screen Initial State 
 		resetb <= '1';
-		clear_screen();
-		draw_walls();
-		
+		clear_screen;
+		draw_walls;
+		draw_paddles;
 
-  
+  --wait until rising_edge(clk);
   --Test 1 | STEP #1
 		--test sgp2 state (PUCK COLLISION and PUCK DRAW)
 		
@@ -350,21 +374,25 @@ begin
 		for i in 0 to 53 loop
 		
 			--Draw ball
-			assert plot_out = '1';
-			assert colour_out = WALL_COLOUR;
-			assert x = puckx;
-			assert y = pucky;
+			assert plot_out = '1' report "plot not 1 CLRSCRN" severity failure;
+			--report "colour_out:"& integer'image( conv_integer(colour_out) ) &" x_out:"&integer'image( conv_integer(x_out) )& " y_out:"& integer'image( conv_integer(y_out) );
+      --report " i:"&integer'image( i );
+			assert colour_out = WALL_COLOUR report "colour_out not WALL_COLOUR (111)" severity failure;
 			puckx := puckx - 1;
 			pucky := pucky - 1;
+			
+			assert x_out = std_logic_vector(puckx);
+			assert y_out = std_logic_vector(pucky);
+
 			wait until rising_edge(clk);
 			
 			--Wait until paddle have been updated
-			assert plot_out = '0';
+			assert plot_out = '0' report "plot!=1 " severity failure;
 			wait until plot_out = '1';
 			
 			--Wait until paddles have been fully drawn
-			for b in 0 to 440 loop
-				assert plot_out = '1';
+			for b in 0 to 441 loop
+				assert plot_out = '1' report "plot != 1 " severity failure;
 				wait until rising_edge(clk);
 			end loop;
 			
@@ -372,8 +400,8 @@ begin
 		
 		
 		-- PUCK hits TOP wall and bounces off DOWNWARD-LEFT  after (26, 6) 
-		assert y = "0000101"; -- 5
-		assert x = "00011011"; -- 25
+		assert y_out = "0000101" report "ynot != 5" severity failure; -- 5
+		assert x_out = "00011011" report "xnot != 25" severity failure; -- 25
 		puckx := puckx - 1;
 		pucky := pucky + 1;
 		vely := '1';
@@ -381,18 +409,18 @@ begin
 		
 		-- PUCK will move on a DOWNWARD-LEFT trajectory till (4, 28) 
 		for i in 0 to 21 loop
-			assert colour_out = WALL_COLOUR;
+			assert colour_out = WALL_COLOUR report "colour_out not WALL_COLOUR (111)" severity failure;
 			puckx := puckx - 1;
 			pucky := pucky + 1;
 			wait until rising_edge(clk);
 			
 			--Wait until paddle have been updated
-			assert plot_out = '0';
+			assert plot_out = '0' report "plot != 0" severity failure;
 			wait until plot_out = '1';
 			
 			--Wait until paddles have been fully drawn
 			for b in 0 to 440 loop
-				assert plot_out = '1';
+				assert plot_out = '1' report "plot != 1" severity failure;
 				wait until rising_edge(clk);
 			end loop;
 		end loop;
@@ -400,23 +428,25 @@ begin
 		
 		-- PUCK hit RIGHT WALL and bounces off DOWNWARD-RIGHT after (4, 28) 
 		-- Player2 scores a point and game resets
-		assert y = "0000101"; -- 5
-		assert x = "00011011"; -- 25
+		assert y_out = "0000101" report "y_out != 5" severity failure; -- 5
+		assert x_out = "00011011" report "x_out != 25" severity failure; -- 25
 		puckx := puckx + 1;
 		pucky := pucky + 1;
 		vely := '1';
 		wait until rising_edge(clk);
 		
 		-- PUCK will be reset to (80,60)
-		clear_screen();
-		draw_walls();
-		draw_paddles();
-		assert x = INIT_X_PUCK; -- 80
-	  assert y = INIT_Y_PUCK; -- 60
+		clear_screen;
+		draw_walls;
+		draw_paddles;
+		assert x_out = std_logic_vector(INIT_X_PUCK) report "x_out != 80" severity failure; -- 80
+	  assert y_out = std_logic_vector(INIT_Y_PUCK) report "y_out != 60" severity failure; -- 60
+	  wait until rising_edge(clk);
 		
+		report "FINISHED TEST#1";
 
     std.env.finish;
 
 end process;
 
-end stimulus;
+end architecture;
